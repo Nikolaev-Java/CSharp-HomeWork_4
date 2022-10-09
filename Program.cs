4ed6e220cb080f943a22d4f9e38cf6e46934f73d
Denis <epselent33@gmail.com>
2022-10-09 18:03:47 +0500

    выполнил третье задание

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 29: Напишите функцию, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33] */
+
+int[] FillArr()
+{
+	int[] arr = new int[8];
+	for (int i = 0; i < arr.Length; i++)
+	{
+		arr[i] = new Random().Next(0, 100);
+	}
+	return arr;
+}
+void PrintArr(int[] _arr)
+{
+	Console.Write("[");
+	foreach (var item in _arr)
+	{
+		Console.Write(item + ", ");
+	}
+	Console.Write("]");
+}
+int[] array;
+array = FillArr();
+PrintArr(array);
