14266d3cedba6c9efe506b66e880215dba50c3eb
Denis <epselent33@gmail.com>
2022-10-09 17:38:20 +0500

    выполнил первое задание

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 25: Напишите функцию, используя цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16 */
+
+int usernumber;
+Console.WriteLine("Введите число");
+while (!int.TryParse(Console.ReadLine(), out usernumber))
+{
+	Console.WriteLine("Не верный ввод. Повторите");
+}
+int userexponent;
+Console.WriteLine("Введите степнь");
+while (!int.TryParse(Console.ReadLine(), out userexponent))
+{
+	Console.WriteLine("Не верный ввод. Повторите");
+}
+int Exponentiation(int _a, int _b)
+{
+	int result = _a;
+	for (int i = 1; i < _b; i++)
+	{
+		result *= _a;
+	}
+	return result;
+}
+Console.WriteLine($"Число {usernumber} в степени {userexponent} равно = {Exponentiation(usernumber, userexponent)}");
